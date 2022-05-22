ca2394b515662dce544bd1ac5c8319861cca88ac
nruffin95 <nruffin95@gmail.com>
2022-05-22 14:21:36 -0500

    Added functions and markdown to generate file

--- Develop/index.js
+++ Develop/index.js
@@ -1,62 +1,82 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
-      {
+const questions = [
+    {
         type: 'input',
-        name: 'name',
+        name: 'Title',
         message: "What is your project title?",
       },
       {
         type: 'input',
-        name: 'description',
-        message: "What is the description of your project?",
+        name: 'Description',
+        message: "What is the project about? Give A detailed description of your project?",
       },
       {
         type: 'input',
-        name: 'installation instructions',
-        message: "What are the installation instructions for your project?",
+        name: 'Table of Contents',
+        message: "Table of contents",
       },
       {
         type: 'input',
-        name: 'usage information',
-        message: "What is the Usage Information for your project?",
+        name: 'installation',
+        message: "What dependencies does the user need to install to run this app?",
       },
       {
         type: 'input',
-        name: 'contribution guidelines',
-        message: "What are the Contribution Guidelines for your project?",
-      },
-      {
-        type: 'input',
-        name: 'test instructions',
-        message: "What are the Test Instructions for your project?",
+        name: 'usage',
+        message: "How is the app used? Give instructions",
       },
       {
         type: 'list',
-        name: 'licences',
-        message: 'Which license would you like to add?',
-        choices: ['Mit License', 'Apache License 2.0', 'Boost Software License', 'I dont want to add a License'],
+        name: 'Licences',
+        message: 'Which license would you like to add? (ie ...MIT)',
+        choices: ['Mit License', 'Apache License 2.0', 'Boost Software License'],
       },
       {
         type: 'input',
-        name: 'github',
+        name: 'Contributing',
+        message: "Who contributed to this project?",
+      },
+      {
+        type: 'input',
+        name: 'Questions',
+        message: "Contact info for inquiries?",
+      },
+      {
+        type: 'input',
+        name: 'Username',
         message: "What is your github user name?",
       },
       {
         type: 'input',
-        name: 'email',
+        name: 'Email',
         message: "What is email address?",
       },
-    ]);
-  };
+]
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log(fileName),
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log('success')
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data){
+            writeToFile("README1.md", generatorMarkdown(data));
+        })
+}
 
 // Function call to initialize app
-promptUser();
+init()
